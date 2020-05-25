153b930ce1cb5f3d0ff44ef8dbf094e285be96e8
Martin Bakke <bakkemartin11@gmail.com>
2020-05-25 22:10:12 +0200

    functionality for 'connecting brush' added
    todo: functioning spray paint

--- app.js
+++ app.js
@@ -15,25 +15,61 @@ window.addEventListener('resize', resize);
 
 // TOOLBAR
 // tool variables
-const brush = document.querySelector('#brush');
+const brush = document.querySelector('.brush-icon');
 const thickness = document.querySelector('#thickness');
 const eraser = document.querySelector('#eraser');
 const colorIcon = document.querySelector('#color');
 const colorPicker = document.querySelector('#hex');
-const bucket = document.querySelector('#bucket');
+
 // tools eventListeners
-brush.addEventListener('click', openBrushes);
+brush.addEventListener('click', toggleBrushes);
 colorIcon.addEventListener('click', () => colorPicker.click());
 colorPicker.addEventListener('change', () => (ctx.strokeStyle = colorPicker.value));
 eraser.addEventListener('click', () => (ctx.strokeStyle = 'white'));
-bucket.addEventListener('click', useBucket);
-// tools functionality
-function openBrushes() {
-	console.log('åpner brush-velger');
+
+// BRUSH TOOL
+// brush stroke variables
+let join = 'round';
+let cap = 'round';
+let rotation;
+let currentBrush = 'paint';
+const paint = document.querySelector('.paint');
+const spray = document.querySelector('.spray');
+const connectingBrush = document.querySelector('.brush-3');
+// get random float for spray paint
+function getRandomFloat(min, max) {
+	return Math.random() * (max - min) + min;
 }
-function useBucket() {
-	console.log('mal alt');
+
+function toggleBrushes() {
+	const dropdown = document.querySelector('.dropdown');
+	dropdown.classList.toggle('active');
 }
+
+//choosing a brush
+paint.addEventListener('click', function() {
+	ctx.restore;
+	currentBrush = 'paint';
+	join = 'round';
+	cap = 'round';
+	toggleBrushes();
+});
+
+spray.addEventListener('click', function() {
+	ctx.restore;
+	currentBrush = 'spray';
+	join = 'round';
+	cap = 'round';
+	toggleBrushes();
+});
+
+connectingBrush.addEventListener('click', function() {
+	ctx.restore;
+	currentBrush = 'connectingBrush';
+	join = 'round';
+	cap = 'round';
+	toggleBrushes();
+});
 
 // DRAWING
 // drawing variables
@@ -41,30 +77,72 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
+// tracker for connecting brush
+let points = [];
+
 // drawing functionality
 function draw(e) {
 	if (!isDrawing) return; //don't run when not moused
 	ctx.lineWidth = thickness.value;
-	ctx.lineJoin = 'round';
-	ctx.lineCap = 'round';
+	ctx.lineJoin = join;
+	ctx.lineCap = cap;
 	//start painting
-	ctx.beginPath();
-	//start from -> go to
-	ctx.moveTo(lastX, lastY);
-	ctx.lineTo(e.offsetX, e.offsetY);
+	if (currentBrush === 'paint') {
+		ctx.restore;
+		ctx.beginPath();
+		//start from -> go to
+		ctx.moveTo(lastX, lastY);
+		ctx.lineTo(e.offsetX, e.offsetY);
 
-	ctx.stroke();
-	//update lastX and lastY
-	lastX = e.offsetX;
-	lastY = e.offsetY;
+		ctx.stroke();
+		//update lastX and lastY
+		lastX = e.offsetX;
+		lastY = e.offsetY;
+	} else if (currentBrush === 'spray') {
+	} else if (currentBrush === 'connectingBrush') {
+		points.push({ x: e.clientX, y: e.clientY });
+		ctx.beginPath();
+		ctx.moveTo(points[points.length - 2].x, points[points.length - 2].y);
+		ctx.lineTo(points[points.length - 1].x, points[points.length - 1].y);
+		ctx.stroke();
+
+		//Get rgb color for strokestyle when connecting lines
+		const rgb = colorPicker.value
+			.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
+			.substring(1)
+			.match(/.{2}/g)
+			.map((x) => parseInt(x, 16));
+
+		// Keep track of previously painted lines
+		for (let i = 0, len = points.length; i < len; i++) {
+			dx = points[i].x - points[points.length - 1].x;
+			dy = points[i].y - points[points.length - 1].y;
+			d = dx * dx + dy * dy;
+			// If previous line is close to our current position, draw a line between them
+			if (d < 1000) {
+				ctx.beginPath();
+				ctx.strokeStyle = `rgba(${rgb.toString()}, 0.2)`;
+				ctx.moveTo(points[points.length - 1].x + dx * 0.2, points[points.length - 1].y + dy * 0.2);
+				ctx.lineTo(points[i].x - dx * 0.2, points[i].y - dy * 0.2);
+				ctx.stroke();
+			}
+		}
+	}
 }
 
 // Draw while mouse is clicked
 canvas.addEventListener('mousedown', (e) => {
 	isDrawing = true;
 	[ lastX, lastY ] = [ e.offsetX, e.offsetY ];
+
+	if (currentBrush === 'connectingBrush') {
+		points.push({ x: e.clientX, y: e.clientY });
+	}
 });
 canvas.addEventListener('mousemove', draw);
 // Don't draw when mouse isn't clicked
 canvas.addEventListener('mouseup', () => (isDrawing = false));
 canvas.addEventListener('mouseout', () => (isDrawing = false));
+
+//Connecting brush
+canvas.addEventListener('mouseup', () => (points.length = 0));
